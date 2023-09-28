815326eacf0a2068e28a8c536ee8c4888bc86175
Babeth Velghe <babeth.velghe@student.hogent.be>
2023-09-28 18:46:06 +0200

    finished lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,21 +9,56 @@ import SwiftUI
 // struct --> structure , kunnen variabelen & function ==> geen klasse !!!
 struct ContentView: View {
     // moet enkel dit hebben
-    let emojis: Array<String> = ["🐵","🐶", "🐸", "🐙"]
+    let emojis: Array<String> = ["🐵","🐶", "🐸", "🐙", "🐥", "🦋", "🐞", "🐯", "🐢", "🐡", "🐴"]
+     @State var cardCount: Int = 4
     // let emojis: [String] = ["🐵","🐶", "🐸", "🐙"]
     var body: some View {
-        HStack {
-            // van zero up to for ..<
-            // van zero up to three ...
-            // geeft waarde terug
-            ForEach(emojis.indices, id: \.self){ index in
-                CardView(content : emojis[index])
+        VStack {
+            ScrollView {
+                cards
             }
-            
+            Spacer()
+            cardCountadjusters
             
         }
         .padding()
-        .foregroundColor(.blue)
+    }
+    var cards : some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            // van zero up to for ..<
+            // van zero up to three ...
+            // geeft waarde terug
+            ForEach(0..<cardCount, id: \.self){ index in
+                CardView(content : emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+
+        }.foregroundColor(.orange)
+    }
+    var cardCountadjusters : some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+            .imageScale(.large)
+            .font(.largeTitle)
+    }
+    // by --> de persoon die deze functie aanroept
+    func cardCountAdjuster(by offset : Int,  symbol : String) -> some View {
+        Button(action : {
+            cardCount += offset
+        }, label: {
+            Image(systemName : symbol)
+        })
+        .disabled(cardCount + offset < 1  ||  cardCount + offset > emojis.count)
+        
+    }
+    var cardRemover : some View {
+         cardCountAdjuster(by: -1, symbol: "minus.circle")
+    }
+    var cardAdder : some View {
+         cardCountAdjuster(by: +1, symbol: "plus.circle")
     }
 }
 
@@ -38,14 +73,12 @@ struct CardView : View {
              let base = RoundedRectangle(cornerRadius: 12 )
             // same thing
             //let base :RoundedRectangle = RoundedRectangle(cornerRadius: 12 )
-            if facedUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 6)
                 Text(content).font(.largeTitle)
-            } else
-            {
-                base.fill()
             }
+            base.fill().opacity(facedUp ? 0 : 1)
            // onTapGesture(count : 2 ) { --> dubbelklik
             // geen viewbuilder -> normaal code (
         }.onTapGesture {
